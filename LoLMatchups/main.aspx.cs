72047d13d15b9746b5f8ffcb16b85fb3d23b7510
Antonite <axp9304@rit.edu>
2015-05-06 03:08:36 -0400

    updated the UI interface, input now recognizes champion names, fixed dababase reader/variable bugs

--- LoLMatchups/main.aspx.cs
+++ LoLMatchups/main.aspx.cs
@@ -12,23 +12,26 @@ using MySql.Data.MySqlClient;
 public partial class _Default : System.Web.UI.Page
 {
     //todo case sensitivity
-    private System.ComponentModel.IContainer components;
     private MySqlConnection connection;
+    private static String summonerId;
     protected void Page_Load(object sender, EventArgs e)
     {
         summoner_id.Text = "";
         status.Text = "";
+        summoner_id.Style.Add("display", "none");
+        champ_as.Style.Add("display", "none");
+        champ_vs.Style.Add("display", "none");
+        champ_button.Style.Add("display", "none");
+        matchupStatus.Text = "";
         connection = connectToServer();
     }
 
     protected void submit(object sender, EventArgs e)
-    {
-
+    {   
         MySqlDataReader reader;
         //search for summoner, Dynamic SQL
         MySqlCommand cmd = new MySqlCommand();
-        cmd.CommandText = "SELECT player.name, " +
-            "player_champion_stat.champion_id," +
+        cmd.CommandText = "SELECT player_champion_stat.champion_id," +
             "player_champion_stat.played_as_won," +
             "player_champion_stat.played_as_total," +
             "player_champion_stat.played_against_won," +
@@ -41,7 +44,7 @@ public partial class _Default : System.Web.UI.Page
                 "lolmatchups.player where lolmatchups.player_champion_stat.summoner_id =" +
                 " lolmatchups.player.summoner_id and lolmatchups.player.name = @name";
         cmd.Parameters.Add("@name", MySqlDbType.VarChar, 11);
-        cmd.Parameters["@name"].Value = box.Text;
+        cmd.Parameters["@name"].Value = summoner_box.Text;
         cmd.CommandType = CommandType.Text;
         cmd.Connection = connection;
 
@@ -55,11 +58,9 @@ public partial class _Default : System.Web.UI.Page
             reader = cmd.ExecuteReader();
 
             //add custom header row
+            //(to be removed upon addition of column names in db)
             TableRow arow = new TableRow();
             TableCell acell = new TableCell();
-            acell.Text = "Summoner Name";
-            arow.Cells.Add(acell);
-            acell = new TableCell();
             acell.Text = "Champion Id";
             arow.Cells.Add(acell);
             acell = new TableCell();
@@ -88,30 +89,100 @@ public partial class _Default : System.Web.UI.Page
             arow.Cells.Add(acell);
 
             infoTable.Rows.Add(arow);
-
             while (reader.Read())
             {
+                
                 arow = new TableRow();
                 for (int i = 0; i < reader.VisibleFieldCount; i++)
                 {
-
                     acell = new TableCell();
                     acell.Text = reader.GetString(i);
                     arow.Cells.Add(acell);
                 }
                 infoTable.Rows.Add(arow);
             }
+
+            summonerId = reader.GetString(5);
+            summoner_id.Text = "";
+            summoner_id.Style.Add("display", "none");
+            infoTable.Style.Add("display", "inline");
         }
         catch (Exception notFound)
         {
-            summoner_id.Text = "Summoner not found";
+            summoner_id.Text = "Summoner " + summoner_box.Text + " not found";
+            summoner_id.Style.Add("display", "inline");
+
+            infoTable.Style.Add("display", "none");
+            champ_as.Style.Add("display", "none");
+            champ_vs.Style.Add("display", "none");
+            champ_button.Style.Add("display", "none");
+
+            connection.Close();
+            return;
         }
 
         //close connection
         connection.Close();
 
         //reset input box
-        box.Text = "";
+        //summoner_box.Text = "";
+
+        champ_as.Style.Add("display", "inline");
+        champ_vs.Style.Add("display", "inline");
+        champ_button.Style.Add("display", "inline");
+    }
+
+    protected void getMatchups (object sender, EventArgs e)
+    {
+        MySqlDataReader reader;
+        //search for summoner, Dynamic SQL
+        MySqlCommand cmd = new MySqlCommand();
+        cmd.CommandText =
+            "SELECT player.name," +
+            "opponent.name," +
+            "player_matchup.won," +
+            "player_matchup.played " +
+            "FROM lolmatchups.player_matchup " +
+            "join lolmatchups.champion player on player_matchup.player_champion_id = player.champion_id " +
+            "join lolmatchups.champion opponent on player_matchup.opponent_champion_id = opponent.champion_id" +
+                " where player.name = @as" +
+                " and opponent.name = @vs" +
+                " and lolmatchups.player_matchup.summoner_id = @id";
+        cmd.Parameters.Add("@as", MySqlDbType.VarChar, 32);
+        cmd.Parameters["@as"].Value = champ_as.Text;
+        cmd.Parameters.Add("@vs", MySqlDbType.VarChar, 32);
+        cmd.Parameters["@vs"].Value = champ_vs.Text;
+        cmd.Parameters.Add("@id", MySqlDbType.VarChar, 8);
+        cmd.Parameters["@id"].Value = summonerId;
+        cmd.CommandType = CommandType.Text;
+        cmd.Connection = connection;
+
+        //open connection
+        try { connection.Open(); }
+        catch (Exception conException) { status.Text = "Did not connect to the Database Server."; }
+
+        //execute
+        try
+        {
+            reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+
+               reader.Read();
+               
+               //calculate win % and diplay
+               float winPercent = reader.GetFloat(2) / reader.GetFloat(3) * 100;
+               matchupStatus.Text = reader.GetString(0) + " vs " + reader.GetString(1) + " " + winPercent + "%";
+
+            }
+            else
+            {
+                matchupStatus.Text = "No data found";
+            }
+        } catch (Exception notFound)
+        {
+            matchupStatus.Text = notFound.ToString();
+        }
     }
 
     protected MySqlConnection connectToServer()
